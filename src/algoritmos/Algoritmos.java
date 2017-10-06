44285670ed9900b73b78b9bfbd8523f5cb01332e
Aluno <Aluno@172.32.1.37>
2017-10-05 21:03:47 -0300

    Criando metodos VOID (sem retorno), e criando outros tipos de métodos com retorno.

--- src/algoritmos/Algoritmos.java
+++ src/algoritmos/Algoritmos.java
@@ -2,8 +2,23 @@ package algoritmos;
 
 public class Algoritmos {
 
+    public static void quadrado(double n) {
+        double r = n * n;
+    }
+
+    public static double metade(double v) {
+        double r = v / 2;
+        return r;
+    }
+
     public static void main(String[] args) {
+        double raiz, res;
+        raiz = Math.sqrt(15);
+        System.out.println(raiz);
         
+        res = metade(2);
+        System.out.println(res);
+
     }
 
 }
